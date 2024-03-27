1b01a10e9d31543001b86c89c6d4f37fcb589d94
ubuntu636 <161006797+ubuntu636@users.noreply.github.com>
2024-03-27 13:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+let s =`
+<h1>Home</h1>
+</br>
+<h1>Page</h1>
+`;
+let sathwik =`
+<h1>Here You Got the Right Page</h1>
+
+<h1>Page</h1>
+`;
+// shavdhjsvdhjvjhsavdjvsvdjvajdvhjavdhjvadhvasjhvjahsdvhjavdhasvhjavhdvahvdhjavjhdva
+app.get('/',(req,res)=>{
+    res.status(200).send(s);
+})
+app.get('/sathwik',(req,res)=>{
+    res.status(200).send(sathwik);
 })
 app.listen(process.env.PORT || 3000)
